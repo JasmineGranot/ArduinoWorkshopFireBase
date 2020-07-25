7d77912e6ca4e1d6307ccc7437f1d4e27639a06b
Vered Harel <veredh12@hotmail.com>
2020-07-25 13:36:02 +0300

    changed the project to java code! added sign in, sign up features (complete) using firebase

---
+++ ArduinoAndroidApplication/app/src/main/java/com/example/arduinoandroidapplication/RegisterActivity.java
@@ -0,0 +1,103 @@
+package com.example.arduinoandroidapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterActivity extends AppCompatActivity {
+    private TextView nameField;
+    private TextView emailField;
+    private TextView phoneField;
+    private TextView ardIdField;
+    private TextView passwordField;
+    private FirebaseAuth auth;
+    private ProgressDialog dialog;
+    private DatabaseReference dbReference;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        nameField = (TextView) findViewById(R.id.registeredName);
+        emailField = (TextView) findViewById(R.id.registeredEmail);
+        phoneField = (TextView) findViewById(R.id.registeredPhoneNumber);
+        ardIdField = (TextView) findViewById(R.id.registeredBraceletId);
+        passwordField = (TextView) findViewById(R.id.registeredPassword);
+        Button registerButton = (Button) findViewById(R.id.registerButton);
+        dialog = new ProgressDialog(this);
+
+        auth = FirebaseAuth.getInstance();
+        dbReference = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRegister();
+            }
+        });
+
+    }
+
+    private void startRegister() {
+        final String userName = nameField.getText().toString().trim();
+        final String userEmail = emailField.getText().toString().trim();
+        final String userPhone = phoneField.getText().toString().trim();
+        final String braceletID = ardIdField.getText().toString().trim();
+        String password = passwordField.getText().toString().trim();
+
+        if(userName.isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "Must Insert User Name!", Toast.LENGTH_SHORT).show();
+        }
+        else if(userEmail.isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "Must Insert User Email!", Toast.LENGTH_SHORT).show();
+        }
+        else if(userPhone.isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "Must Insert User Phone!", Toast.LENGTH_SHORT).show();
+        }
+        else if(braceletID.isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "Must Insert Bracelet ID!", Toast.LENGTH_SHORT).show();
+        }
+        else if(password.isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "Must Insert Password!", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            dialog.setMessage("Signing Up...");
+            dialog.show();
+            auth.createUserWithEmailAndPassword(userEmail, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()) {
+                        String userId = auth.getCurrentUser().getUid();
+                        DatabaseReference currentUserDb = dbReference.child(userId);
+                        currentUserDb.child("name").setValue(userName);
+                        currentUserDb.child("email").setValue(userEmail);
+                        currentUserDb.child("phoneNumber").setValue(userPhone);
+                        currentUserDb.child("braceletId").setValue(braceletID);
+
+                        dialog.dismiss();
+
+                        Intent mainIntent = new Intent(RegisterActivity.this, ScreenMainActivity.class);
+                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(mainIntent);
+                    }
+                }
+            });
+        }
+    }
+}
