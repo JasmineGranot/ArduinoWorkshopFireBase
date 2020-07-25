7fd8a210d2bc9971ffb609ca5c6ecb5ea9f2dec7
Vered Harel <veredh12@hotmail.com>
2020-07-25 17:17:33 +0300

    trying to add notifications from pulse change

--- ArduinoAndroidApplication/app/src/main/java/com/example/arduinoandroidapplication/PulseHistory.java
+++ ArduinoAndroidApplication/app/src/main/java/com/example/arduinoandroidapplication/PulseHistory.java
@@ -1,8 +1,14 @@
 package com.example.arduinoandroidapplication;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,6 +16,7 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,6 +33,7 @@ public class PulseHistory extends AppCompatActivity {
     private TableLayout layout;
     private TableRow tableRow;
     private TextView firstText, secondText;
+    private Map<String, String> map = new HashMap<>();
     TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
 
     @Override
@@ -34,11 +42,18 @@ public class PulseHistory extends AppCompatActivity {
         setContentView(R.layout.activity_pulse_history);
 
         DatabaseReference dbref = FirebaseDatabase.getInstance().getReference().child("100/pulse_history");
-        ValueEventListener valueEventListener = new ValueEventListener() {
+
+        dbref.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Map<String, String> map = new HashMap<>();
-                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                map.put(snapshot.getKey(), snapshot.child("heart_rate").getValue().toString());
+                addDataToTable(map);
+                notification("Anomaly pulse");
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                for(DataSnapshot ds : snapshot.getChildren()) {
                     String date = ds.getKey();
                     String heartBeat = ds.child("heart_rate").getValue().toString();
                     map.put(date, heartBeat);
@@ -48,11 +63,40 @@ public class PulseHistory extends AppCompatActivity {
             }
 
             @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
-        };
-        dbref.addValueEventListener(valueEventListener);
+        });
+
+//        ValueEventListener valueEventListener = new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                Map<String, String> map = new HashMap<>();
+//                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+//                    String date = ds.getKey();
+//                    String heartBeat = ds.child("heart_rate").getValue().toString();
+//                    map.put(date, heartBeat);
+//                }
+//
+//                addDataToTable(map);
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        };
+//        dbref.addValueEventListener(valueEventListener);
     }
 
     private void addDataToTable(Map<String, String> map) {
@@ -75,5 +119,21 @@ public class PulseHistory extends AppCompatActivity {
             tableRow.addView(secondText);
             layout.addView(tableRow);
         }
+    }
+
+    private void notification(String msg){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("n", "n", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "n")
+                .setContentText(String.format("New %s detected!", msg))
+                .setAutoCancel(true)
+                .setPriority( NotificationCompat.PRIORITY_MAX);
+
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        managerCompat.notify(999, builder.build());
     }
 }
