840cfae134e2f23af99a0ad5bd8f3658ef245638
Vered Harel <veredh12@hotmail.com>
2020-09-08 12:38:43 +0300

    started new fall detection
    new sensor

---
+++ ArduinoCode/freeFallDetection/freeFallDetection.ino
@@ -0,0 +1,150 @@
+#include <Wire.h>
+#include "ESP8266WiFi.h"
+#include <MPU6050.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+#include <FirebaseArduino.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+void ICACHE_RAM_ATTR DoSomething();
+
+
+MPU6050 mpu;
+
+//================================ Wifi ================================
+WiFiServer  server(80);
+//char        ssid[]      = "Oslo";            // your network SSID name
+//char        pass[]      = "0526586120";      // your network password
+char        ssid[]      = "HOTBOX 4-BC50";            // your network SSID name
+char        pass[]      = "0542005293";      // your network password
+//================================ Wifi ================================
+
+boolean freefallDetected = false;
+
+void setup() 
+{
+  Serial.begin(115200);
+  // Connect to WIFI service:
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("Initialize MPU6050");
+
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_16G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+
+  mpu.setAccelPowerOnDelay(MPU6050_DELAY_3MS);
+  
+  mpu.setIntFreeFallEnabled(true);
+  mpu.setIntZeroMotionEnabled(false);
+  mpu.setIntMotionEnabled(false);
+  
+  mpu.setDHPFMode(MPU6050_DHPF_5HZ);
+
+  mpu.setFreeFallDetectionThreshold(17);
+  mpu.setFreeFallDetectionDuration(1);  
+  
+  checkSettings();
+  
+  attachInterrupt(12, DoSomething, RISING);
+}
+
+void doInt()
+{
+  freefallDetected = true;  
+}
+
+void checkSettings()
+{
+  Serial.println();
+  
+  Serial.print(" * Sleep Mode:                ");
+  Serial.println(mpu.getSleepEnabled() ? "Enabled" : "Disabled");
+
+  Serial.print(" * Motion Interrupt:     ");
+  Serial.println(mpu.getIntMotionEnabled() ? "Enabled" : "Disabled");
+
+  Serial.print(" * Zero Motion Interrupt:     ");
+  Serial.println(mpu.getIntZeroMotionEnabled() ? "Enabled" : "Disabled");
+
+  Serial.print(" * Free Fall Interrupt:       ");
+  Serial.println(mpu.getIntFreeFallEnabled() ? "Enabled" : "Disabled");
+
+  Serial.print(" * Free Fal Threshold:          ");
+  Serial.println(mpu.getFreeFallDetectionThreshold());
+
+  Serial.print(" * Free FallDuration:           ");
+  Serial.println(mpu.getFreeFallDetectionDuration());
+  
+  Serial.print(" * Clock Source:              ");
+  switch(mpu.getClockSource())
+  {
+    case MPU6050_CLOCK_KEEP_RESET:     Serial.println("Stops the clock and keeps the timing generator in reset"); break;
+    case MPU6050_CLOCK_EXTERNAL_19MHZ: Serial.println("PLL with external 19.2MHz reference"); break;
+    case MPU6050_CLOCK_EXTERNAL_32KHZ: Serial.println("PLL with external 32.768kHz reference"); break;
+    case MPU6050_CLOCK_PLL_ZGYRO:      Serial.println("PLL with Z axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_YGYRO:      Serial.println("PLL with Y axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_XGYRO:      Serial.println("PLL with X axis gyroscope reference"); break;
+    case MPU6050_CLOCK_INTERNAL_8MHZ:  Serial.println("Internal 8MHz oscillator"); break;
+  }
+  
+  Serial.print(" * Accelerometer:             ");
+  switch(mpu.getRange())
+  {
+    case MPU6050_RANGE_16G:            Serial.println("+/- 16 g"); break;
+    case MPU6050_RANGE_8G:             Serial.println("+/- 8 g"); break;
+    case MPU6050_RANGE_4G:             Serial.println("+/- 4 g"); break;
+    case MPU6050_RANGE_2G:             Serial.println("+/- 2 g"); break;
+  }  
+
+  Serial.print(" * Accelerometer offsets:     ");
+  Serial.print(mpu.getAccelOffsetX());
+  Serial.print(" / ");
+  Serial.print(mpu.getAccelOffsetY());
+  Serial.print(" / ");
+  Serial.println(mpu.getAccelOffsetZ());
+
+  Serial.print(" * Accelerometer power delay: ");
+  switch(mpu.getAccelPowerOnDelay())
+  {
+    case MPU6050_DELAY_3MS:            Serial.println("3ms"); break;
+    case MPU6050_DELAY_2MS:            Serial.println("2ms"); break;
+    case MPU6050_DELAY_1MS:            Serial.println("1ms"); break;
+    case MPU6050_NO_DELAY:             Serial.println("0ms"); break;
+  }  
+  
+  Serial.println();
+}
+
+void loop()
+{
+}
+
+void DoSomething()
+{
+  freefallDetected = true; 
+  Vector rawAccel = mpu.readRawAccel();
+  Activites act = mpu.readActivites();
+
+    if (freefallDetected)
+    {
+    Serial.println("Detected Fall!");
+    delay(100);
+    }
+
+   freefallDetected = false;
+    
+}
